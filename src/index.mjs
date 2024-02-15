16afd270efb548242817ccee9709bbbc780baaa7
Michael Lesniak <mail@mlesniak.com>
2024-02-15 10:53:26 +0100

    CI/CD working?

--- src/index.mjs
+++ src/index.mjs
@@ -2,6 +2,6 @@ export const handler = async (event) => {
     console.log(event);
     return {
         statusCode: 200,
-        body: new Date() + " --- updated",
+        body: new Date() + " --- updated from workflow",
     };
 };
